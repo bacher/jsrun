afa4aca39d66bb9f9cd6d47c8d0edd8ee3f8a1a1
Ilya Lebedev <bacherko@gmail.com>
2022-09-27 08:29:04 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
 import fs from 'fs';
+import { takeRightWhile } from 'lodash';
 
 import { parseJs } from './parser/parser';
 import { runSourceFile } from './engine/jsrun';
+
+const argParams: Record<string, boolean> = {};
+
+console.log('ARGV', process.argv0);
+
+for (const str of takeRightWhile(process.argv, (str) => str.startsWith('--'))) {
+  argParams[str.replace(/^--/, '')] = true;
+}
 
 const sourceCode = fs.readFileSync('example/program.js', 'utf-8');
 
@@ -9,9 +18,10 @@ console.log('===== PARSE =====');
 
 const program = parseJs(sourceCode);
 
-console.log('=====  RUN  =====');
-
-runSourceFile(program);
+if (!argParams.parse) {
+  console.log('=====  RUN  =====');
+  runSourceFile(program);
+}
 
 const programJson = JSON.stringify(program.statements, null, 2);
 
